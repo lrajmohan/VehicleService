fb987c62383d9e6dc1c36cace6050bee2ea00073
lrajmohan <rajmohan.logu@gmail.com>
2014-04-06 13:34:42 -0400

    Yabba mukkiyamana doc- without any error after PF Query and successful logout

--- VehicleService/finalUIViewController.m
+++ VehicleService/finalUIViewController.m
@@ -30,7 +30,7 @@
 
 - (IBAction)Logout:(id)sender {
     [PFUser logOut];
-   // [self dismissViewControllerAnimated:YES completion:nil];
-    [self performSegueWithIdentifier:@"firstscreen" sender:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
+  //  [self performSegueWithIdentifier:@"firstscreen" sender:nil];
 }
 @end
